e716e3aa73861dbb542b9635f1c38f4f1d9c20a4
Tim Webster <tim.webster@contino.io>
2021-08-28 11:53:29 +0100

    add nsg for adds

--- vnet.tf
+++ vnet.tf
@@ -20,6 +20,66 @@ resource "azurerm_subnet" "adds-sn" {
   address_prefixes     = [var.cidr_adds_subnet]
 }
 
+# AD DS NSG
+resource "azurerm_network_security_group" "adds" {
+  name                = "adds-nsg"
+  location            = azurerm_resource_group.avd-vnet.location
+  resource_group_name = azurerm_resource_group.avd-vnet.name
+
+  security_rule {
+    name                       = "AllowSyncWithAzureAD"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "AzureActiveDirectoryDomainServices"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowRD"
+    priority                   = 201
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "CorpNetSaw"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowPSRemoting"
+    priority                   = 301
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5986"
+    source_address_prefix      = "AzureActiveDirectoryDomainServices"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowLDAPS"
+    priority                   = 401
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "636"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource azurerm_subnet_network_security_group_association "adds-nsg-assoc" {
+  subnet_id                 = azurerm_subnet.adds-sn.id
+  network_security_group_id = azurerm_network_security_group.adds.id
+}
+
 
 
 
