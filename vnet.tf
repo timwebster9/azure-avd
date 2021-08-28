ae8992963545d30de299c6c37e178bf27ff40540
Tim Webster <tim.webster@contino.io>
2021-08-28 10:18:22 +0100

    fix

--- vnet.tf
+++ vnet.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "avd" {
   name                = "avd-vnet"
   location            = azurerm_resource_group.avd-vnet.location
   resource_group_name = azurerm_resource_group.avd-vnet.name
-  address_space       = var.cidr_avd_vnet
+  address_space       = [var.cidr_avd_vnet]
 }
 
 # SUBNETS
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "adds-sn" {
   name                 = "adds-sn"
   resource_group_name  = azurerm_resource_group.avd-vnet.name
   virtual_network_name = azurerm_virtual_network.avd.name
-  address_prefixes     = var.cidr_adds_subnet
+  address_prefixes     = [var.cidr_adds_subnet]
 }
 
 
